a8b73792c60ee0cfd9be533df411cdd290af9629
Juanesteban462 <juanesteban462@hotmail.com>
2022-06-29 22:51:03 -0500

    C: Se realizan las pruebas unitarias del servicio de booking

--- src/test/java/com/user/service/ImplBookingServiceTest.java
+++ src/test/java/com/user/service/ImplBookingServiceTest.java
@@ -4,11 +4,8 @@ import com.user.model.Booking;
 import com.user.repository.IBookingRepository;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.util.AssertionErrors.assertEquals;
-import static org.springframework.test.util.AssertionErrors.assertNull;
 
 class ImplBookingServiceTest {
 
@@ -16,7 +13,6 @@ class ImplBookingServiceTest {
 
     @Test
     void get_information_create_book() {
-
         //Arrange
         IBookingRepository objIBookingRepository = Mockito.mock(IBookingRepository.class);
         objImplBookingService = new ImplBookingService(objIBookingRepository);
@@ -43,11 +39,6 @@ class ImplBookingServiceTest {
     }
 
     @Test
-    void no_get_book_when_call_find_by_idBook() {
-
-    }
-
-    @Test
     void get_books_when_call_find_all_books() {
         //Arrange
         IBookingRepository objIBookingRepository = Mockito.mock(IBookingRepository.class);
@@ -62,10 +53,30 @@ class ImplBookingServiceTest {
 
     @Test
     void updateBook() {
+        //Arrange
+        IBookingRepository objIBookingRepository = Mockito.mock(IBookingRepository.class);
+        objImplBookingService = new ImplBookingService(objIBookingRepository);
 
+        Booking objBooking = new Booking("Don Quijote de la Mancha","Novela","Miguel de cervantes",100);
+
+        //Act
+        objImplBookingService.updateBook(objBooking);
+
+        //Assert
+        verify(objIBookingRepository).save(objBooking);
     }
 
     @Test
     void deleteBook() {
+
+        //Arrange
+        IBookingRepository objIBookingRepository = Mockito.mock(IBookingRepository.class);
+        objImplBookingService = new ImplBookingService(objIBookingRepository);
+
+        //Act
+        objImplBookingService.deleteBook(1000L);
+
+        //Assert
+        verify(objIBookingRepository).deleteById(1000L);
     }
 }
